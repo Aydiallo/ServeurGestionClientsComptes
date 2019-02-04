6b2a1836b277e706e73bfce8e6ee444882e5e8ab
aydiallo <diallo.alpha-yankhouba@ugb.edu.sn>
2019-02-04 10:41:30 +0000

    first commit

---
+++ src/main/java/com/gestion/ServeurGestionClientsComptesApplication.java
@@ -0,0 +1,14 @@
+package com.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionClientsComptesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionClientsComptesApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/gestion/ServeurGestionClientsComptesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gestion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServeurGestionClientsComptesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
